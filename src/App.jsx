0ffe4929bf01b504a6a782c625a0d05a8c89f223
minjun <minjun23221@gmail.com>
2024-12-01 00:59:45 +0900

    지도, 검색, 마커 기능 구현

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,9 @@ function App() {
 
   return (
     <>
-<div className="flex items-center justify-center h-screen bg-cover bg-center" style={{ backgroundImage: `url('/img/main_img.jpg')` }}>
-  <h1 className="text-white text-4xl font-bold bg-black bg-opacity-50 p-4 rounded-lg">Tailwind 확인용</h1>
-</div>
       <Router />
     </>
-  )
+  );
 }
 
 export default App
