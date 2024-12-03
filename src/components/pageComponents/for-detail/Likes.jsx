8acf3b134478c96957ee10a5451ce4530aedfea6
ParkGana <gnchoco97@gmail.com>
2024-12-03 14:14:40 +0900

    feat : Detail 페이지 컴포넌트 분리

---
+++ src/components/pageComponents/for-detail/Likes.jsx
@@ -0,0 +1,9 @@
+const Likes = () => {
+    return (
+        <p className="text-2xl text-black font-bold">
+            <span className="text-2xl text-red-500 font-bold">♥</span> 5
+        </p>
+    );
+};
+
+export default Likes;
