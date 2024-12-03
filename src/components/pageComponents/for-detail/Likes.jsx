925d0abf2fbf3b930bf9a4790e25bd73af45af78
ParkGana <gnchoco97@gmail.com>
2024-12-03 15:31:06 +0900

    feat : 좋아요 추가 및 삭제

--- src/components/pageComponents/for-detail/Likes.jsx
+++ src/components/pageComponents/for-detail/Likes.jsx
@@ -5,10 +5,15 @@ import { useLikes } from '../../../hooks/useLikes';
 const Likes = () => {
     const { id: spotId } = useParams();
     const { user } = useUser();
-    const { likes } = useLikes({ userId: user.id, placeId: spotId });
+    const { likes, createMutation, deleteMutation } = useLikes({ userId: user.id, placeId: spotId });
+
+    /* 좋아요 추가 및 삭제 */
+    const handleToggle = () => {
+        likes.isLiked ? deleteMutation.mutate({ userId: user.id, placeId: spotId }) : createMutation.mutate({ userId: user.id, placeId: spotId });
+    };
 
     return (
-        <p className="text-2xl text-black font-bold cursor-pointer">
+        <p className="text-2xl text-black font-bold cursor-pointer" onClick={handleToggle}>
             <span className="text-2xl text-red-500 font-bold">{likes?.isLiked ? '♥' : '♡'}</span> {likes?.count}
         </p>
     );
