1eb44a2c90febe7f5ce6ec2868f59985c08ba72b
림졍 <mara2277@naver.com>
2024-12-03 09:04:57 +0900

    feat: Nav 수정 및 footer 추가/supabse 설치 및 Client 추가(+ 이미지 수정)

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import HeroSection from "./components/pageComponents/ui/HeroSection";
 import Router from "./shared/Router";
 
 function App() {
   return (
     <>
-      <HeroSection />
+
       <Router />
     </>
   );
--- src/components/pageComponents/ui/Footer.jsx
+++ src/components/pageComponents/ui/Footer.jsx
@@ -1,8 +1,22 @@
-
 const Footer = () => {
   return (
-    <div>
-      푸터
+    <div className="relative w-full h-[250px] bg-[#D9D9D9] mx-auto">
+      {/* Home, Bookmark, Youtube */}
+      <div className="absolute left-[9.17%] top-[27.2%] flex flex-col space-y-4 text-[#484848] text-[18px] leading-[21px]">
+        <div>Home</div>
+        <div>Bookmark</div>
+        <div>Youtube</div>
+      </div>
+
+      {/* 이용 약관 */}
+      <div className="absolute left-[33.33%] top-[58%] text-[#484848] text-[18px] leading-[21px]">
+        이용 약관
+      </div>
+
+      {/* ⓒ copyright by Yu-Gi-0h! */}
+      <div className="absolute left-[71.33%] top-[58%] text-right text-[#484848] text-[18px] leading-[21px]">
+        ⓒ copyright by Yu-Gi-0h!
+      </div>
     </div>
   )
 }
