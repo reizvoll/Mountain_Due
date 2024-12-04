21232c4a14956ba19cdc4871cfcfd6d6fd115092
ParkGana <gnchoco97@gmail.com>
2024-12-04 15:02:36 +0900

    feat : 좋아요, 리뷰 부분에 낙관적 업데이트 적용

--- src/hooks/useLikes.js
+++ src/hooks/useLikes.js
@@ -13,13 +13,51 @@ export const useLikes = ({ userId, placeId }) => {
     /* like 추가 */
     const createMutation = useMutation({
         mutationFn: createLikeAPI,
-        onSuccess: () => queryClient.invalidateQueries(['likes'])
+        // onSuccess: () => queryClient.invalidateQueries(['likes'])
+        onMutate: async () => {
+            await queryClient.cancelQueries({
+                queryKey: ['likes']
+            });
+
+            const prevLikes = queryClient.getQueryData(['likes']);
+
+            queryClient.setQueryData(['likes'], (prev) => ({ count: prev.count + 1, isLiked: true }));
+
+            return { prevLikes };
+        },
+        onError: (context) => {
+            queryClient.setQueryData(['likes'], context.prevLikes);
+        },
+        onSettled: () => {
+            queryClient.invalidateQueries({
+                queryKey: ['likes']
+            });
+        }
     });
 
     /* like 삭제 */
     const deleteMutation = useMutation({
         mutationFn: deleteLikeAPI,
-        onSuccess: () => queryClient.invalidateQueries(['likes'])
+        // onSuccess: () => queryClient.invalidateQueries(['likes'])
+        onMutate: async () => {
+            await queryClient.cancelQueries({
+                queryKey: ['likes']
+            });
+
+            const prevLikes = queryClient.getQueryData(['likes']);
+
+            queryClient.setQueryData(['likes'], (prev) => ({ count: prev.count - 1, isLiked: false }));
+
+            return { prevLikes };
+        },
+        onError: (context) => {
+            queryClient.setQueryData(['likes'], context.prevLikes);
+        },
+        onSettled: () => {
+            queryClient.invalidateQueries({
+                queryKey: ['likes']
+            });
+        }
     });
 
     return { likes, createMutation, deleteMutation };
