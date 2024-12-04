53cc874b98bbafcb7e5819856802488fae036753
minjun <qkralswns1998@gmail.com>
2024-12-04 19:50:37 +0900

    fix: 버그 수정.

--- src/api/signup.js
+++ src/api/signup.js
@@ -15,7 +15,7 @@ export const signUpUser = async ({ email, password }) => {
 export const uploadProfileImage = async (image) => {
   const fileName = `${uuidv4()}`; // UUID를 사용한 고유 파일명 생성
   const folderPath = "users"; // 폴더 경로 설정
-  console.log(folderPath, fileName)
+  console.log(folderPath, fileName);
   // 이미지 업로드
   const { data: uploadData, error: uploadError } = await supabase.storage
     .from("images") // 버킷 이름
@@ -27,13 +27,15 @@ export const uploadProfileImage = async (image) => {
 
   // 아오 에러 뭔데... get publicurl error...
   // 업로드된 이미지의 공개 URL 가져오기
-  const { publicUrl } = await supabase.storage
-  .from("images")
+  const {
+    data: { publicUrl },
+  } = await supabase.storage
+    .from("images")
 
-  .getPublicUrl(`${folderPath}/${fileName}`);
-if (!publicUrl) {
-  throw new Error("이미지 URL 생성 실패");
-}
+    .getPublicUrl(`${folderPath}/${fileName}`);
+  if (!publicUrl) {
+    throw new Error("이미지 URL 생성 실패");
+  }
 
   console.log("생성된 이미지 URL:", publicUrl);
   return publicUrl; // URL 반환
