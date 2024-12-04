f7f4dbabde0d46ab693bb50f02cc36b62f575dc5
minjun <minjun23221@gmail.com>
2024-12-04 20:27:01 +0900

    fix: Map수정

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import useUser from "../../hooks/useUser";
+import Map from "./Map";
 
 const Home = () => {
   const { user, isLoggedIn, logout } = useUser();
@@ -6,6 +7,7 @@ const Home = () => {
   console.log("users 리덕스 훅 체크용==>", user, isLoggedIn);
   return (
     <div>
+      <Map />
       비회원 전용 메인 페이지
       <button onClick={logout}>로그 아웃</button>
     </div>
