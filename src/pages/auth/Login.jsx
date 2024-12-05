1e96dc135794ae64a072f38fb462b771731b0e6b
dongeul24 <gogo0232@naver.com>
2024-12-05 11:39:46 +0900

    fix: 네비 고침

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -70,6 +70,9 @@ const Login = () => {
           img_url: user.img_url,
         })
       );
+
+    // 로그인 시, 바로 직전 페이지로 이동하도록 구현
+      showToast("로그인에 성공했습니다!", "success", () => navigate("/"));
       
     } catch (error) {
       showToast("알 수 없는 오류가 발생했습니다. 다시 시도해주세요.", "error");
